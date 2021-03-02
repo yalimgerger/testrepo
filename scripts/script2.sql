ae1a06421ff0662ec8a5d0d99b7645b12087e847
admin <admin>
2021-03-02 09:53:23 +0300

    hhhjh

--- scripts/script2.sql
+++ scripts/script2.sql
@@ -1,1 +1,59 @@
-22w
+Founded in 2005, Gerger is a products and services company for smart enterprise IT departments. With customers in more than 40 countries, we have an excellent track record of delivering the right solution at the right time and budget.
+
+We are the creators of Formspider, the application development tool for Oracle PL/SQL developers, Gitora, the source control tool for Oracle  and ProHuddle, the webinar network for Oracle customers.
+
+Our boutique consulting arm has delivered many projects such as industry specific web and mobile apps, business rules engines, data model designers, ETL tools, Cloud migrations and meta-data management solutions for large private and government organizations as well as small and medium size businesses around the world.
+
+Our notable customers include companies such as DHL, US Air Force, TD Bank, Babcock, Rhenus, NN Insurance and HSBC.
+
+In the last 15 years, thanks to the high quality of our products, success of our customers and integrity of our message, we’ve earned the trust of the Oracle community and built relationships with thousands of developers, decision makers and influencers in the Oracle ecosystem.
+
+We welcome the opportunity to work with every organisation which aims to empower PL/SQL developers. Having more platforms to build on is beneficial to our community because it creates new opportunities for PL/SQL developers.
+ 
+Partnership Opportunities
+
+Our (hopefully correct) assumption is that TMax aims to introduce its database Tibero to more Oracle customers. This can be achieved with a top down (by engaging with executives, CIO’s and other high level decision makers...) and a bottom up (by engaging with IT directors, IT managers, medium to small size software vendors/consulting companies, influencers, senior developers, etc..) approach.
+
+We can help with the bottom up adoption by building developer applications that attract the PL/SQL community and evangelizing Tibero database in the community.
+
+A few of the actions we can take are listed below:
+Evangelize Tibero to Oracle Customers
+We have a network of 10,000+ Oracle users. This network consists of Formspider customers, Gitora customers and ProHuddle webinar network members.
+
+With our websites, products and online events we can help you reach your target audience with the right message.
+
+We can use our relationships with our customers, community members and influencers to introduce Tibero to more Oracle customers.
+
+Fill the Gaps in TMax Product Line Up
+Here is a quick matrix that shows only a small portion of the gaps:
+
+Commencing on the purchase or renewal of a support term (“Effective Date”), We will supply the Services to Customer with respect to the Software described in Section 1 below, for which We have granted a development license to Customer pursuant to a software license agreement (the “Software License Agreement”) by and between Gerger and Customer and for whom the applicable support fee has been paid.
+1. Included Support Services
+Support plan explained below applies only to the Gerger’s Software Product trademarked as Formspider (“Software”), for which they are purchased and do not apply to any other application, library or product developed or sold by Us. The support, if any, for any other such product will be governed by a separate agreement. All support features listed below are available only for the support term plus any extensions or renewals, and such support features will become unavailable immediately upon termination of this Agreement.
+
+The support plan includes the following basic features:
+Web access to the most current documentation available (the “Documentation”), including tutorials and examples, access to the most current version for the Software. Unlimited, 24×7 access to the community-driven Gerger public support forums.
+2. Metered Support Services
+All support features listed in this document are metered in 15 minute increments and are only available up to the amount of support time (“Time”) available in the Customer’s support account at the time of the support incident. Additional Time may be purchased and spent on additional support. All references to “cost” in this section mean the duration of Time spent in delivering the service. All references to “billing” in this section mean the deduction of accumulated Time from the support account of the Customer.
+a. Support Requests
+Support requests submitted to the Gerger are valued per single related incident. Each incident costs a minimum of 15 minutes. Any additional Time required to resolve the incident will be deducted from the Customer’s Support Account at the sole discretion of Gerger support personnel. If telephone support is also required to resolve the incident, the cost for the telephone support is added separately and billed in addition to the incident support cost under the terms of Section 2b. Support requests determined to be unrelated to the original request shall constitute a new support incident at the sole discretion of Gerger support personnel.
+b. Telephone Support
+Telephone support is valued in 15-minute increments, with a 15 minute minimum cost per call (even if the call does not have an actual duration of 15 minutes). The number of separate phone calls is limited only by the Time available in the Customer’s support account balance. Telephone support must be scheduled in advance so that Gerger and the Customer can establish mutually agreeable availability.
+c. Expert Services
+Expert services include, but are not limited to, remote trouble-shooting on the Customer’s computer systems, performance tuning and code review of the Customer’s application code by Gerger support staff. Expert services are valued in 15-minute increments, with a 15 minute minimum cost  (even if the service does not have an actual duration of 15 minutes). All requests for expert services must be scheduled in advance so that Gerger and the Customer can establish mutually-agreeable availability.
+3. Terms of Support
+a. Hours of Availability
+Access to all web-based support features (online documentation and downloadable Software, online help forums and private issue tracking system) are generally available 24 hours per day, 7 days a week, 365 days a year barring unforeseen interruptions in Internet service or planned exceptions by Gerger. Notwithstanding anything to the contrary in this Agreement, Gerger does not guarantee such availability. All metered support services are also generally available during business hours (Monday-Friday 09:00-18:00 Istanbul time), although the response to each request for metered support may not happen immediately. Any planned exceptions to the availability of support services will be communicated by Gerger to the Customer as far in advance as possible.
+b. Our Responsibilities
+We will use commercially reasonable efforts to assist Customer to resolve problems in its use of the Software as described in Section 1. We make no guarantee that it can, or will, solve any problems with respect to the Software presented by Customer, and further disclaims any warranties above and beyond any limited warranties that may have been expressly made by Us in the Software License Agreement.
+We will offer Services for the then-current version of the Software (Version N). For not less than twelve (12) months from the release date of the then current version, We will also offer Services for the major version immediately preceding the then-current version (Version N-1) of the Software.
+c. Customer’s Responsibilities
+Customer is responsible for all hardware, operating systems, network setup, network maintenance and setup and use of any file access control systems required in the support of the Software. Customer may be required to grant Us certain limited access rights to Customer’s proprietary computer systems in order that We may render support Services.
+Customer is responsible for ensuring that its personnel have sufficient training to attain and maintain competence in the operation of the Software. If We determine, in Our sole discretion, in responding to a Customer request for Support, that the solution is provided in available media (including, but not limited to, the source code documentation, tutorials and examples, websites or support forums), We may direct Customer’s personnel to the appropriate media for the solution to the problem. Even if the support request were resolved in so doing, any minimum support charges outlined in section 2 would still apply.
+4. Subscription Services
+During the Services Term, and provided Customer shall have continuously subscribed to the Services, Gerger will provide Customer access to Private Releases, if any, as well as corresponding Documentation of the Software for development purposes. “Private Release” means a release of the Software that contains functional enhancements or extensions, that is not publicly available at the time of sharing with the Customer. The modifications and enhancements in Private Releases may be publicly available at a later time.
+5. Term and Termination
+Our provision of the Services to Customer will commence on the Effective Date and will continue for an initial term of one (1) year (the “Initial Term”). The Services will automatically terminate at the end of the Initial Term, unless Customer chooses to renew Services, subject to Gerger’s written agreement (each such renewal also referred to as a “Renewal Term”).
+“Services Term” shall mean the Initial Term as extended by each Renewal Term.
+Time purchased must be used during the term in which it is purchased, and at the end of the Initial Term and each Renewal Term, any accumulated Time for that term that has not been spent will be lost. At the end of the Services Term the Services automatically terminate and any accumulated Time that has not been spent will be lost.
+Gerger may update Services terms after the Initial Term with thirty (30) days advance notice to Customer of any changes deemed by Gerger to have potential adverse and/or material impacts to the Customer. Upon any renewal, Gerger’s then current terms and conditions for the Services will apply.
